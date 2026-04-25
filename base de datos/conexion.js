a6396428cc50e854e98b49719c628c5f620cb546
santiagotuberquia1805 <santiagotuberquia1805@gmail.com>
2026-04-25 08:16:07 -0500

    Add files via upload

--- base de datos/conexion.js
+++ base de datos/conexion.js
@@ -1,10 +1,4 @@
 // conexion.js
-
-// Definimos la ruta de nuestro servidor local de productos
-const API_URL = "http://localhost:3000/api";
-
-// Lo compartimos con los demás archivos JS
+const API_URL = "https://tienda-de-belleza.onrender.com/api";
 window.API_URL = API_URL;
-
-// Mensaje de éxito para que sepas que el "puente" está listo
 console.log("✅ Frontend conectado al servidor en: " + API_URL);
